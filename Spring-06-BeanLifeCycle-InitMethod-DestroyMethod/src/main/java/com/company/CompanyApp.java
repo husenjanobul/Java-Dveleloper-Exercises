0cf8b3735b0729b0c26be493b74cc8a57dc9b8aa
husenjanobul <husenjanobul@gmail.com>
2021-02-16 20:55:21 +0100

    Spring BeanLifeCycle- myInitMethod and myDestroyMethod

---
+++ Spring-06-BeanLifeCycle-InitMethod-DestroyMethod/src/main/java/com/company/CompanyApp.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import com.company.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CompanyApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course = container.getBean("java", Course.class);
+        course.getTeachingHours();
+
+        ((ClassPathXmlApplicationContext)container).close();
+
+    }
+
+}
