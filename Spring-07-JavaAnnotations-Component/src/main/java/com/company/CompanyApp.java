360c4a138cda6d525a425687915643b8d5d65fdf
husenjanobul <husenjanobul@gmail.com>
2021-02-16 23:15:30 +0100

    Java Annotations - Component

---
+++ Spring-07-JavaAnnotations-Component/src/main/java/com/company/CompanyApp.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import com.company.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CompanyApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course = container.getBean("API", Course.class);
+        course.getTeachingHours();
+
+
+    }
+
+}
---
+++ Spring-07-JavaAnnotations-Component/src/main/java/com/company/services/Java.java
@@ -0,0 +1,14 @@
+package com.company.services;
+
+import com.company.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Java implements Course {
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours: 30");
+    }
+
+}
