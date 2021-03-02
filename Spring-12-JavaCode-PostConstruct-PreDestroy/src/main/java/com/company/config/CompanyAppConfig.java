b6cb1ca29bc112efde3bcafe36293778c14cf5ad
husenjanobul <husenjanobul@gmail.com>
2021-03-02 23:00:34 +0100

    PostConstruct - PreDestroy

--- Spring-12-JavaCode-PostConstruct-PreDestroy/src/main/java/com/company/config/CompanyAppConfig.java
+++ Spring-12-JavaCode-PostConstruct-PreDestroy/src/main/java/com/company/config/CompanyAppConfig.java
@@ -1,7 +1,5 @@
 package com.company.config;
 
-import com.company.services.Java;
-import com.company.services.Selenium;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
--- Spring-12-JavaCode-PostConstruct-PreDestroy/src/main/java/com/company/services/Java.java
+++ Spring-12-JavaCode-PostConstruct-PreDestroy/src/main/java/com/company/services/Java.java
@@ -1,19 +1,11 @@
 package com.company.services;
 
 import com.company.interfaces.Course;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Component
 public class Java implements Course {
-
-    @Override
-    public void getTeachingHours() {
-        System.out.println("Weekly teaching hours: 30") ;
-    }
-
     @PostConstruct
     public void postConstruct(){
         System.out.println("Executing Post Construct");
@@ -24,4 +16,8 @@ public class Java implements Course {
         System.out.println("Executing PreDestroy");
     }
 
+    @Override
+    public void getTeachingHours() {
+
+    }
 }
