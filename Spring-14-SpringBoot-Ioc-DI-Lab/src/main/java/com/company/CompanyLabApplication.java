ac17729f6344704695452df4ef2af60a52a3a474
husenjanobul <husenjanobul@gmail.com>
2021-03-05 23:43:59 +0100

    SpringBoot Lab - Carpet Calculator

--- Spring-14-SpringBoot-Ioc-DI-Lab/src/main/java/com/company/CompanyLabApplication.java
+++ Spring-14-SpringBoot-Ioc-DI-Lab/src/main/java/com/company/CompanyLabApplication.java
@@ -1,13 +1,21 @@
 package com.company;
 
+import com.company.calculator.Calculator;
+import com.company.enums.City;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class CompanyLabApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompanyLabApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		ApplicationContext container = SpringApplication.run(CompanyLabApplication.class, args);
+
+		Calculator calculator = container.getBean("calculator",Calculator.class);
+		System.out.println(calculator.getTotalCarpetCost(City.DULLES));
+
+
 	}
 
 }
