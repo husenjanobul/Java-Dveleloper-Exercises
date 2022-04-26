3ce6aecb46956cde7681a5a41813265a121c32b5
husenjanobul <husenjanobul@gmail.com>
2022-04-26 12:57:47 +0200

    SpringBoot- DI ConstructorInjection

--- SpringBoot-03-DI-ConstructorInjection/src/main/java/com/company/services/Java.java
+++ SpringBoot-03-DI-ConstructorInjection/src/main/java/com/company/services/Java.java
@@ -1,10 +1,17 @@
 package com.company.services;
 
 import com.company.interfaces.Course;
+import com.company.interfaces.ExtraSessions;
 
 public class Java implements Course {
+
+    ExtraSessions extraSessions;
+    public Java(OfficeHours officeHours) {
+        this.extraSessions = officeHours;
+    }
+
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours: 30");
+        System.out.println("Weekly teaching hours: " + (30+ extraSessions.getHours()));
     }
 }
