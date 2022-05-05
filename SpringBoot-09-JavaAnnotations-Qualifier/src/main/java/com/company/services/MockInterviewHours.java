1e824caefcc7d8307d1b855cad76217a5a8b3796
husenjanobul <husenjanobul@gmail.com>
2022-05-05 19:47:03 +0200

    SpringBoot- JavaAnnotation - Qualifier

---
+++ SpringBoot-09-JavaAnnotations-Qualifier/src/main/java/com/company/services/MockInterviewHours.java
@@ -0,0 +1,12 @@
+package com.company.services;
+
+import com.company.interfaces.ExtraSessions;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MockInterviewHours implements ExtraSessions {
+    @Override
+    public int getHours() {
+        return 7;
+    }
+}
