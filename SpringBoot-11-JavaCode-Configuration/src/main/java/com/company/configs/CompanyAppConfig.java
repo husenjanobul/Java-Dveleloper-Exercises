e8ade1434d926255f2f4b999a1343bd76e75b149
husenjanobul <husenjanobul@gmail.com>
2022-05-10 18:56:27 +0200

    SpringBoot- JavaCode - Configuration / ComponentScan / @Bean

--- SpringBoot-11-JavaCode-Configuration/src/main/java/com/company/configs/CompanyAppConfig.java
+++ SpringBoot-11-JavaCode-Configuration/src/main/java/com/company/configs/CompanyAppConfig.java
@@ -1,9 +1,44 @@
 package com.company.configs;
 
+import com.company.interfaces.ExtraSessions;
+import com.company.services.Java;
+import com.company.services.OfficeHours;
+import com.company.services.Selenium;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.company")
+@PropertySource("classpath:application.properties")
 public class CompanyAppConfig {
+
+    @Bean
+//    public Java java(){
+//        return new Java(officeHours());
+//    }
+    public Java java(){
+        return new Java(extraSessions());
+    }
+
+    @Bean
+    public Selenium selenium(){
+        return new Selenium();
+    }
+
+    /*
+    @Bean
+    public OfficeHours officeHours(){
+        return new OfficeHours();
+    }
+
+     */
+
+    public ExtraSessions extraSessions(){
+        return new OfficeHours();
+    }
+
+
+
 }
--- SpringBoot-11-JavaCode-Configuration/src/main/java/com/company/services/Java.java
+++ SpringBoot-11-JavaCode-Configuration/src/main/java/com/company/services/Java.java
@@ -1,13 +1,43 @@
 package com.company.services;
 
 import com.company.interfaces.Course;
+import com.company.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.util.Arrays;
+
+//@Component
 public class Java implements Course {
+
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours: 30");
+        System.out.println("Weekly teaching hours: " + (30+extraSessions.getHours()));
+    }
+
+    @Value("JD1")
+    private String batch;
+
+    @Value("${instructor}")
+    private String instructor;
+
+    @Value("${days}")
+    private String[] days;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", days=" + Arrays.toString(days) +
+                ", extraSessions=" + extraSessions +
+                '}';
     }
 
 }
