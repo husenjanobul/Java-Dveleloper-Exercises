9fba5b3338381d7d79f6fdcc6f4e4d65b1430639
husenjanobul <husenjanobul@gmail.com>
2022-05-18 16:01:15 +0200

    SpringBoot - MVC - RequestMapping

--- SpringBoot-15-SpringBoot-MVC-Controller/src/main/java/com/company/controller/HomeController.java
+++ SpringBoot-15-SpringBoot-MVC-Controller/src/main/java/com/company/controller/HomeController.java
@@ -1,10 +1,7 @@
 package com.company.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -21,19 +18,43 @@ public class HomeController {
         return "person";
     }
 
-    @RequestMapping(method = RequestMethod.GET,value = "/husenjan")
-    public String getRequestMapping3(){
-        return "person";
-    }
+//    @RequestMapping(method = RequestMethod.GET,value = "/husenjan")
+//    public String getRequestMapping3(){
+//        return "person";
+//    }
 
     @GetMapping( "/husenjan")
     public String getRequestMapping4(){
         return "person";
     }
 
-    @PostMapping( "/husenjan")
-    public String postRequestMapping(){
-        return "person";
+//    @PostMapping( "/husenjan")
+//    public String postRequestMapping(){
+//        return "person";
+//    }
+
+    @GetMapping("/home/{name}")
+    public String pathVariable(@PathVariable("name") String name){
+        System.out.println("name is: " + name);
+        return "home";
+    }
+
+    @GetMapping("/home/{name}/{email}")
+    public String pathVariable2(@PathVariable("name") String name,@PathVariable("email") String email){
+        System.out.println("name is: " + name);
+        System.out.println("email is: " + email);
+        return "home";
+    }
+
+    @GetMapping("/home/course")
+    public String requestMapping(@RequestParam("course") String name){
+        System.out.println("name is: " +name);
+        return "home";
+    }
+    @GetMapping("/course")
+    public String requestMapping2(@RequestParam(value = "course",required = false,defaultValue = "company") String name){
+        System.out.println("name is: " +name);
+        return "home";
     }
 
 
