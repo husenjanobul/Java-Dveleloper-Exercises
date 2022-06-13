02ec520eb87f3f6dedfdda2aea1eeb3a376109f6
husenjanobul <husenjanobul@gmail.com>
2022-06-13 21:50:35 +0200

    SpringBoot - ORM - @Column / @Transient / @Temporal

--- SpringBoot-18-ORM-TableCreation/src/main/java/com/company/entity/Student.java
+++ SpringBoot-18-ORM-TableCreation/src/main/java/com/company/entity/Student.java
@@ -1,6 +1,10 @@
 package com.company.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "students")
@@ -10,8 +14,32 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // generate id automatically
     private Long studentId;
 
+    @Column(name = "studentFirstName") //change column name
     private String firstName;
     private String lastName;
     private String email;
+
+    @Transient // hide column , doesn't exist in the table
+    private String city;
+
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    @Temporal(TemporalType.TIME)
+    private Date birthTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthDateTime;
+
+//java8
+    private LocalDate localDate;
+    private LocalTime localTime;
+    private LocalDateTime localDateTime;
+//or
+    @Column(columnDefinition = "DATE")
+    private LocalDate localDate1;
+    @Column(columnDefinition = "TIME")
+    private LocalTime localTime1;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime1;
 
 }
