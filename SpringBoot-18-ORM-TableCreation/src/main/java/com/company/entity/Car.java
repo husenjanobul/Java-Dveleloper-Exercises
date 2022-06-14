d16fc5c8d6f3c833487d117fc352ba7a5301389e
husenjanobul <husenjanobul@gmail.com>
2022-06-14 17:25:14 +0200

    SpringBoot - ORM - @Repository

--- SpringBoot-18-ORM-TableCreation/src/main/java/com/company/entity/Car.java
+++ SpringBoot-18-ORM-TableCreation/src/main/java/com/company/entity/Car.java
@@ -1,9 +1,17 @@
 package com.company.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity // create class name table "car"
 @Table(name = "cars") //create "cars" table
+@NoArgsConstructor
+@Getter
+@Setter
 public class Car {
 
     @Id // primary key
@@ -12,4 +20,9 @@ public class Car {
 
     private String make;
     private String model;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
 }
