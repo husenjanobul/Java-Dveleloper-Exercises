29a81799e977108196c98f113d3d1a95b4476718
husenjanobul <husenjanobul@gmail.com>
2022-06-28 22:08:30 +0200

    naming

---
+++ SpringBoot-21-ORM-Mapping-OneToMany/src/main/java/com/company/SpringBoot19OrmMappingOneToManyApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot19OrmMappingOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot19OrmMappingOneToManyApplication.class, args);
+	}
+
+}
