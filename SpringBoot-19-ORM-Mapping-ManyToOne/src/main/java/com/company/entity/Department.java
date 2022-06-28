a862dc2d8040b3103cc3f3e461543433da0c6406
husenjanobul <husenjanobul@gmail.com>
2022-06-28 21:32:48 +0200

    SpringBoot - ORM - MneToOne

---
+++ SpringBoot-19-ORM-Mapping-ManyToOne/src/main/java/com/company/entity/Department.java
@@ -0,0 +1,28 @@
+package com.company.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Department extends BaseEntity {
+
+
+    private String department;
+    private String division;
+
+
+    public Department(String department, String division) {
+        this.department = department;
+        this.division = division;
+    }
+}
