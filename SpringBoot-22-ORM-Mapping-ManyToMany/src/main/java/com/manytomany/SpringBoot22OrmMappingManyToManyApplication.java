ddfa3c8ed309f7177132cbdd7157412cc19d3ab2
husenjanobul <husenjanobul@gmail.com>
2022-07-18 21:37:30 +0200

    SpringBoot - ORM - ManyToMany

---
+++ SpringBoot-22-ORM-Mapping-ManyToMany/src/main/java/com/manytomany/SpringBoot22OrmMappingManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.manytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot22OrmMappingManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot22OrmMappingManyToManyApplication.class, args);
+	}
+
+}
