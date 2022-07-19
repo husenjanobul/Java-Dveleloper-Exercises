0d1a8a99de4d5401da6dbe163f665ffa6c9c6bdd
husenjanobul <husenjanobul@gmail.com>
2022-07-19 21:46:04 +0200

    SpringBoot - ORM - ManyToMany

---
+++ SpringBoot-22-ORM-Mapping-ManyToMany/src/main/java/com/manytomany/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.manytomany.repository;
+
+import com.manytomany.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
