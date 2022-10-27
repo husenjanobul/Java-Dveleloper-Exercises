5acc1a2e15078702318313dd84c7b250a2e5227e
husenjanobul <husenjanobul@gmail.com>
2022-10-27 22:03:57 +0200

    Spring - Security Custom Configuration

---
+++ SpringBoot-27-Security-CustomConfiguration/src/main/java/com/company/SpringBoot27SecurityCustomConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot27SecurityCustomConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot27SecurityCustomConfigurationApplication.class, args);
+	}
+
+}
