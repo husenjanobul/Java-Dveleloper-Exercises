0c0eab1563cda55761f0c4997aefd7783a0a280c
husenjanobul <husenjanobul@gmail.com>
2022-10-30 21:17:29 +0100

    Spring - Security HttpBasic AuthRoleBase

--- SpringBoot-28-Security-HttpBasic-AuthRoleBase/src/main/java/com/company/security/SecurityConfiguration.java
+++ SpringBoot-28-Security-HttpBasic-AuthRoleBase/src/main/java/com/company/security/SecurityConfiguration.java
@@ -17,6 +17,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.
                 authorizeRequests() //requests should be authorized
+                .antMatchers("index.html").permitAll()
+                .antMatchers("profile/**").authenticated()
+                .antMatchers("admin/**").hasRole("ADMIN")
+                .antMatchers("management/**").hasAnyRole("ADMIN","MANAGER")
                 .anyRequest().authenticated() //incoming request should be authenticated
                 .and()
                 .httpBasic();  // perform basic http authentication
@@ -26,13 +30,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.
                 inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin123"))
-                .roles("ADMIN")
+                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
                 .and()
-                .withUser("husenjan")
-                .password(passwordEncoder().encode("obul123"))
-                .roles("USER");
+                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER")
+                .and()
+                .withUser("husenjan").password(passwordEncoder().encode("obul123")).roles("USER");
     }
 
     @Bean
