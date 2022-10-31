d04ff094933f350ac3bf050435c251e67a0ad1e0
husenjanobul <husenjanobul@gmail.com>
2022-10-31 21:56:45 +0100

    Spring - Security HttpBasic Database

---
+++ SpringBoot-29-Security-HttpBasic-Database/src/main/java/com/company/SpringBoot29SecurityHttpBasicDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot29SecurityHttpBasicDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot29SecurityHttpBasicDatabaseApplication.class, args);
+	}
+
+}
