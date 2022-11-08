333731b61efd427449d31180b9026cbf0ac17776
husenjanobul <husenjanobul@gmail.com>
2022-11-08 21:28:33 +0100

    Spring - Security FormAuth

---
+++ SpringBoot-30-Security-FormAuth/src/test/java/com/compay/SpringBoot30SecurityFormAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot30SecurityFormAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
