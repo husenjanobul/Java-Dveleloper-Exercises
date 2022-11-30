641607abd767564201b2beb0ad1ee1d5adc0008d
husenjanobul <husenjanobul@gmail.com>
2022-11-30 21:40:03 +0100

    Spring - Rest-ResponseEntity , HttpHeaders

---
+++ SpringBoot-33-Rest-ResponseEntity/src/main/java/com/company/controller/ProductController.java
@@ -0,0 +1,55 @@
+package com.company.controller;
+
+import com.company.entity.Product;
+import com.company.service.ProductService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public Product getProduct(@PathVariable("id") long id){
+        return productService.getProduct(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts(){
+
+        HttpHeaders responseHttpHeaders = new HttpHeaders();
+        responseHttpHeaders.set("Version","Company.v1");
+        responseHttpHeaders.set("Operation","Get List");
+
+        return ResponseEntity
+                .ok()
+                .headers(responseHttpHeaders)
+                .body(productService.getProducts());
+    }
+
+    @PostMapping
+    public List<Product> createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public List<Product> deleteProduct(@PathVariable("id") long id){
+        return productService.delete(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    public List<Product> updateProduct(@PathVariable("id") long id,@RequestBody Product product){
+        return productService.updateProduct(id,product);
+    }
+
+}
