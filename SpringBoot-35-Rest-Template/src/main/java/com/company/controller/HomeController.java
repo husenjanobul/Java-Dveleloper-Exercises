ad29e5f35423d034df60c9f59dec369038334d6c
husenjanobul <husenjanobul@gmail.com>
2023-01-24 20:53:27 +0100

    rest template

--- SpringBoot-35-Rest-Template/src/main/java/com/company/controller/HomeController.java
+++ SpringBoot-35-Rest-Template/src/main/java/com/company/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.company.controller;
 import com.company.entity.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -30,6 +31,19 @@ public class HomeController {
     public Object readUser(@PathVariable("id") Integer id){
         String URL = URI + "/{id}";
         return restTemplate.getForObject(URL,Object.class,id);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostsFromDummyApi(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        httpHeaders.set("app-id","lTE5abbDxdjGplutvTuc");
+
+        HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET,entity,Object.class);
+
+        return response;
+
     }
 
 
