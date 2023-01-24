cf47dd58c6423ad2af3e513e46eb0c7f9329a11a
husenjanobul <husenjanobul@gmail.com>
2023-01-24 15:55:43 +0100

    rest template

--- SpringBoot-35-Rest-Template/src/main/java/com/company/SpringBoot35RestTemplateApplication.java
+++ SpringBoot-35-Rest-Template/src/main/java/com/company/SpringBoot35RestTemplateApplication.java
@@ -1,7 +1,13 @@
 package com.company;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class SpringBoot35RestTemplateApplication {
@@ -10,4 +16,11 @@ public class SpringBoot35RestTemplateApplication {
         SpringApplication.run(SpringBoot35RestTemplateApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
+    }
 }
