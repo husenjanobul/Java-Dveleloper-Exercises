1b59c6e4297652379fb54013ac165f90a9f72a69
husenjanobul <husenjanobul@gmail.com>
2023-09-05 22:55:49 +0200

    Dependency injection - constructor injection and annotations - Autowired

--- Spring-review-01-Ioc/src/main/java/stereotype_annotations/Model/DataStructure.java
+++ Spring-review-01-Ioc/src/main/java/stereotype_annotations/Model/DataStructure.java
@@ -1,12 +1,22 @@
 package stereotype_annotations.Model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataStructure {
 
+//    @Autowired
+//    ExtraHours extraHours;
+
+    ExtraHours extraHours;
+
+    public DataStructure(ExtraHours extraHours) {
+        this.extraHours = extraHours;
+    }
+
     public void getTotalHours(){
-        System.out.println("Total hours: " + 34);
+        System.out.println("Total hours: " + (34 + extraHours.getHours()));
     }
 
 }
---
+++ Spring-review-01-Ioc/src/main/java/stereotype_annotations/Model/ExtraHours.java
@@ -0,0 +1,12 @@
+package stereotype_annotations.Model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExtraHours {
+
+    public Integer getHours(){
+        return 10;
+    }
+
+}
