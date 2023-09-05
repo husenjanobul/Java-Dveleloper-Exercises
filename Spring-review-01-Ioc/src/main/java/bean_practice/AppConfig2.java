a828a3304d2bdd7785efeae4ac28025869ef019f
husenjanobul <husenjanobul@gmail.com>
2023-09-05 22:23:42 +0200

    annotations - Configuration, Bean, @Primary

---
+++ Spring-review-01-Ioc/src/main/java/bean_practice/AppConfig2.java
@@ -0,0 +1,26 @@
+package bean_practice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class AppConfig2 {
+
+    @Bean(name = "WelcomeTo")
+    public String welcome(){
+        return "Welcome to my app";
+    }
+
+    @Bean
+    public String  practice(){
+        return "Spring core practice";
+    }
+
+    @Primary
+    @Bean
+    public String  test(){
+        return "Primary test";
+    }
+
+}
--- Spring-review-01-Ioc/src/main/java/bean_practice/CompanyApp.java
+++ Spring-review-01-Ioc/src/main/java/bean_practice/CompanyApp.java
@@ -8,13 +8,26 @@ public class CompanyApp {
     public static void main(String[] args) {
 
         //create spring container
-        ApplicationContext container = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext container = new AnnotationConfigApplicationContext(AppConfig.class,AppConfig2.class);
 
         FullTimeEmployee ft = container.getBean(FullTimeEmployee.class);
         ft.createAccount();
 
         PartTimeEmployee pt = container.getBean(PartTimeEmployee.class);
         pt.createAccount();
+
+//        String welcome = container.getBean("welcome",String.class);
+//        System.out.println(welcome);
+
+        String welcome2 = container.getBean("WelcomeTo",String.class);
+        System.out.println(welcome2);
+
+        String practice = container.getBean("practice",String.class);
+        System.out.println(practice);
+
+        String primary = container.getBean(String.class);
+        System.out.println(primary);
+
 
     }
 
