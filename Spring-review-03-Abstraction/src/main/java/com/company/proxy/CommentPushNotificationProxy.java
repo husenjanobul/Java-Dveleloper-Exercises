1ce20d7b470bd634c31c826ab57962c582c92f61
husenjanobul <husenjanobul@gmail.com>
2023-11-07 21:39:23 +0100

    qualifier

--- Spring-review-03-Abstraction/src/main/java/com/company/proxy/CommentPushNotificationProxy.java
+++ Spring-review-03-Abstraction/src/main/java/com/company/proxy/CommentPushNotificationProxy.java
@@ -2,16 +2,14 @@ package com.company.proxy;
 
 import com.company.model.Comment;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Primary
-@Qualifier("email")
-public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+@Qualifier("push")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
 
     @Override
     public void sendComment(Comment comment) {
-        System.out.println("Sending notification for comment : " + comment.getText());
+        System.out.println("Sending push notification for comment: " + comment.getText());
     }
 }
