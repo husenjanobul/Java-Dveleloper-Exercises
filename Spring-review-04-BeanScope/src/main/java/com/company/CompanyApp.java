d46680c35244d1fed521271d8d9c2186bd1ec2bb
husenjanobul <husenjanobul@gmail.com>
2023-11-07 21:56:19 +0100

    bean scope

---
+++ Spring-review-04-BeanScope/src/main/java/com/company/CompanyApp.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import com.company.config.ProjectConfig;
+import com.company.model.Comment;
+import com.company.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CompanyApp {
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Husenjan");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+        System.out.println("cs1 = " + cs1);
+        System.out.println("cs2 = " + cs2);
+
+    }
+}
