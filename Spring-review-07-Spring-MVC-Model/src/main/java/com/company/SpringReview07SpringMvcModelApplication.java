eefd944bfa56e7a8fadfd8a3d91f0ef54fbaa01c
husenjanobul <husenjanobul@gmail.com>
2023-11-08 23:27:15 +0100

    Spring MVC - Model

---
+++ Spring-review-07-Spring-MVC-Model/src/main/java/com/company/SpringReview07SpringMvcModelApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReview07SpringMvcModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReview07SpringMvcModelApplication.class, args);
+	}
+
+}
---
+++ Spring-review-07-Spring-MVC-Model/src/main/java/com/company/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping("/welcome")
+    public String homePage(Model model){
+
+        model.addAttribute("name","Husenjan");
+        model.addAttribute("course","MVC");
+
+        return "/student/welcome";
+    }
+
+}
