2b53ca60ea7f1ac769b02a33d21932fc53a83b35
husenjanobul <husenjanobul@gmail.com>
2023-11-08 18:03:49 +0100

    SpringBoot Demo

--- Spring-review-05-SpringBoot-Demo/src/main/java/com/example/springreview05springbootdemo/CompanyApplication.java
+++ Spring-review-05-SpringBoot-Demo/src/main/java/com/example/springreview05springbootdemo/CompanyApplication.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.example.springreview05springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompanyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompanyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyApplication.class, args);
+    }
 
 }
