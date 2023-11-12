4eb7e52140674f5dce9d15a0b95c121c09f93cf9
husenjanobul <husenjanobul@gmail.com>
2023-11-12 22:39:58 +0100

    Spring MVC - Model

--- Spring-review-07-Spring-MVC-Model/src/main/java/com/company/controller/StudentController.java
+++ Spring-review-07-Spring-MVC-Model/src/main/java/com/company/controller/StudentController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -12,6 +16,20 @@ public class StudentController {
 
         model.addAttribute("name","Husenjan");
         model.addAttribute("course","MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        int studentId = new Random().nextInt(1000);
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(14);
+        numbers.add(23);
+        numbers.add(54);
+        numbers.add(7);
+        model.addAttribute("numbers",numbers);
 
         return "/student/welcome";
     }
