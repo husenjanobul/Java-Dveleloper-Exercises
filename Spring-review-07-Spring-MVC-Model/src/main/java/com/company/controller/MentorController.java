fe950667353f39c7cf19575977e1de4f09ab3c47
husenjanobul <husenjanobul@gmail.com>
2023-11-12 23:12:35 +0100

    Spring MVC - Model

---
+++ Spring-review-07-Spring-MVC-Model/src/main/java/com/company/controller/MentorController.java
@@ -0,0 +1,39 @@
+package com.company.controller;
+
+import com.company.enums.Gender;
+import com.company.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MentorController {
+
+    @RequestMapping("/mentor")
+    public String mentor(Model model){
+
+        Mentor mentor1 = new Mentor("Abdullah","Obul",42, Gender.MALE);
+        Mentor mentor2 = new Mentor("Abdurahman","Obul",35, Gender.MALE);
+        Mentor mentor3 = new Mentor("Nuramine","Ablimit",56, Gender.FEMALE);
+
+        model.addAttribute("mentor1",mentor1);
+        model.addAttribute("mentor2",mentor2);
+        model.addAttribute("mentor3",mentor3);
+
+
+        //or
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Abdullah","Obul",42, Gender.MALE));
+        mentorList.add(new Mentor("Abdurahman","Obul",35, Gender.MALE));
+        mentorList.add(new Mentor("Nuramine","Ablimit",56, Gender.FEMALE));
+
+        model.addAttribute("mentors",mentorList);
+
+        return "mentor/mentor-list";
+    }
+
+}
