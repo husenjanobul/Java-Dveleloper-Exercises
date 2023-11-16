0e278dec9801858ccdce183d8e72fa962790bfbe
husenjanobul <husenjanobul@gmail.com>
2023-11-16 20:01:25 +0100

    Spring MVC - Thymeleaf

--- Spring-review-08-Spring-Thymeleaf/src/main/java/com/company/Controller/StudentController.java
+++ Spring-review-08-Spring-Thymeleaf/src/main/java/com/company/Controller/StudentController.java
@@ -17,4 +17,12 @@ public class StudentController {
         return "student/register";
     }
 
+    @RequestMapping("/welcome")
+    public String info(){
+
+
+
+        return "student/welcome";
+    }
+
 }
