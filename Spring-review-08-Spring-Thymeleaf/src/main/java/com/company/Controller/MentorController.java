e79941eb44816829165b6cd88bcdb631fa315f62
husenjanobul <husenjanobul@gmail.com>
2023-11-19 13:07:04 +0100

    Spring MVC - Thymeleaf - PostMapping, ModelAttribute

--- Spring-review-08-Spring-Thymeleaf/src/main/java/com/company/Controller/MentorController.java
+++ Spring-review-08-Spring-Thymeleaf/src/main/java/com/company/Controller/MentorController.java
@@ -4,6 +4,8 @@ import com.company.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -22,6 +24,16 @@ public class MentorController {
         model.addAttribute("batchList",batchList);
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String confirmForm(@ModelAttribute("mentor") Mentor mentor, Model model){
+
+//        model.addAttribute("mentor",new Mentor());
+
+        System.out.println("mentor = " + mentor.toString());
+
+        return "mentor/mentor-confirmation";
     }
 
 
