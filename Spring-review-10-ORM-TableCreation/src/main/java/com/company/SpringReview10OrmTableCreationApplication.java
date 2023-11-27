65d5901d160494bb648cc46cab0cb161fadc621a
husenjanobul <husenjanobul@gmail.com>
2023-11-27 21:51:14 +0100

    ORM - part1

---
+++ Spring-review-10-ORM-TableCreation/src/main/java/com/company/SpringReview10OrmTableCreationApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReview10OrmTableCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReview10OrmTableCreationApplication.class, args);
+	}
+
+}
