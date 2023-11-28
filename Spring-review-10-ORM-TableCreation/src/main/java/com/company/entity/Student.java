a01dc02b7b0f1d205084a1a88f040ec3db61f428
husenjanobul <husenjanobul@gmail.com>
2023-11-28 19:55:47 +0100

    ORM - @Table , @Column(name/columnDefinition)  , @Enumerated(EnumType.STRING)

--- Spring-review-10-ORM-TableCreation/src/main/java/com/company/entity/Student.java
+++ Spring-review-10-ORM-TableCreation/src/main/java/com/company/entity/Student.java
@@ -1,18 +1,38 @@
 package com.company.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.company.enums.Gender;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "students")  // created table name is users
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // postgres automatically add id
     private Long id;
     private String firstName;  // field name in table first_name
+
+    @Column(name = "studentFirstName")  // created table column name
     private String lastName;
     private String email;
+
+    @Transient  // this field(column) doesn't create in table
+    private String city;
+
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthdate;
+
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
 }
