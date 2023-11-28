ebbb920f65e12558e4544ed9473822ed2467194c
husenjanobul <husenjanobul@gmail.com>
2023-11-28 16:59:13 +0100

    ORM - @GeneratedValue

--- Spring-review-10-ORM-TableCreation/src/main/java/com/company/entity/Student.java
+++ Spring-review-10-ORM-TableCreation/src/main/java/com/company/entity/Student.java
@@ -1,12 +1,15 @@
 package com.company.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // postgres automatically add id
     private Long id;
     private String firstName;  // field name in table first_name
     private String lastName;
--- Spring-review-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-review-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO EMPLOYEES(id,name)
-VALUES(1,'Mike Smith');
+-- INSERT INTO EMPLOYEES(id,name)
+-- VALUES(1,'Mike Smith');
 
 
-INSERT INTO student(id,first_name,last_name,email)
-VALUES(1,'Mike', 'Smith','mike@gmail.com');
+INSERT INTO student(first_name,last_name,email)
+VALUES('Mike', 'Smith','mike@gmail.com');
