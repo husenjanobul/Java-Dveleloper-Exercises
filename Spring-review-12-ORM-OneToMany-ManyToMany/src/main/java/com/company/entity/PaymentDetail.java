81a59c1aea8210a2f70ce5a8bdb8b7c2d6151bbd
husenjanobul <husenjanobul@gmail.com>
2023-11-29 22:17:03 +0100

    ORM - OneToOne/cascade

---
+++ Spring-review-12-ORM-OneToMany-ManyToMany/src/main/java/com/company/entity/PaymentDetail.java
@@ -0,0 +1,34 @@
+package com.company.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payment_details")
+@NoArgsConstructor
+@Data
+public class PaymentDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal merchantPayoutAmount;
+    private BigDecimal commissionAmount;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate payoutDate;
+
+    @OneToOne(mappedBy = "paymentDetail",cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public PaymentDetail(BigDecimal merchantPayoutAmount, BigDecimal commissionAmount, LocalDate payoutDate) {
+        this.merchantPayoutAmount = merchantPayoutAmount;
+        this.commissionAmount = commissionAmount;
+        this.payoutDate = payoutDate;
+    }
+}
