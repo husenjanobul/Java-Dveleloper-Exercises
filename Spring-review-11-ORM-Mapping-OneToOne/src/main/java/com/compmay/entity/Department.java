c7de89fb421c59bd798300d1d609414f737bdced
husenjanobul <husenjanobul@gmail.com>
2023-11-29 20:43:37 +0100

    ORM - @OneToOne/@OneToOne(mappedBy="...."), @JoinColumn

--- Spring-review-11-ORM-Mapping-OneToOne/src/main/java/com/compmay/entity/Department.java
+++ Spring-review-11-ORM-Mapping-OneToOne/src/main/java/com/compmay/entity/Department.java
@@ -2,6 +2,7 @@ package com.compmay.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,9 @@ public class Department extends BaseEntity {
 
     private String department;
     private String division;
+
+    @OneToOne(mappedBy = "department")
+    private Employee employee;
 
     public Department(String department, String division) {
         this.department = department;
