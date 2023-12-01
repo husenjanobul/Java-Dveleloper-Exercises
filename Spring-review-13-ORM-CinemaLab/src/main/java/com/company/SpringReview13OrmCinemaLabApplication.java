19e93cdd370133b483c76a7c820f602f9083fac3
husenjanobul <husenjanobul@gmail.com>
2023-12-01 10:36:12 +0100

    ORM - CinemaLab

---
+++ Spring-review-13-ORM-CinemaLab/src/main/java/com/company/SpringReview13OrmCinemaLabApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReview13OrmCinemaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReview13OrmCinemaLabApplication.class, args);
+    }
+
+}
---
+++ Spring-review-13-ORM-CinemaLab/src/main/java/com/company/enums/MovieType.java
@@ -0,0 +1,7 @@
+package com.company.enums;
+
+public enum MovieType {
+
+    PREMIER,REGULAR;
+
+}
