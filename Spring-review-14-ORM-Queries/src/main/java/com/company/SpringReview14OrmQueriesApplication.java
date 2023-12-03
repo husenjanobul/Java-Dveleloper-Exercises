a149a3c65615052d23c87ab8470bfd7b0e6bdf16
husenjanobul <husenjanobul@gmail.com>
2023-12-03 20:35:27 +0100

    ORM - Queries

---
+++ Spring-review-14-ORM-Queries/src/main/java/com/company/SpringReview14OrmQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReview14OrmQueriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReview14OrmQueriesApplication.class, args);
+    }
+
+}
