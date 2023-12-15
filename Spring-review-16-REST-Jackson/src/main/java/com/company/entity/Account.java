29c1cdd13f4e46f102b6ebc3015b2d086555e423
husenjanobul <husenjanobul@gmail.com>
2023-12-15 11:16:42 +0100

    REST- @JsonIgnoreProperties / @JsonManagedReference / @JsonBackReference

--- Spring-review-16-REST-Jackson/src/main/java/com/company/entity/Account.java
+++ Spring-review-16-REST-Jackson/src/main/java/com/company/entity/Account.java
@@ -4,18 +4,17 @@ import com.company.enums.UserRole;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @Table(name = "account_details")
-@JsonIgnoreProperties(value = {"hibernateLazyInitializer"},ignoreUnknown = true)
+@JsonIgnoreProperties(value = {"state","postalCode"},ignoreUnknown = true)
 public class Account extends BaseEntity {
 
     private String name;
@@ -32,7 +31,7 @@ public class Account extends BaseEntity {
     private UserRole role = UserRole.USER;
 
     @OneToOne(mappedBy = "account")
-    @JsonBackReference //is the back part of reference - it will be omitted from serialization
+    @JsonBackReference //is the back part of reference - it will be omitted from serialization - user will be ignored
     private User user;
 
 }
