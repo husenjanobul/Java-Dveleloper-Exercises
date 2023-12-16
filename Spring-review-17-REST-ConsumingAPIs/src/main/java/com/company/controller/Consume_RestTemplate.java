49b3d543fde2d29520997cd243d90191fb6fbb44
husenjanobul <husenjanobul@gmail.com>
2023-12-16 22:16:09 +0100

    REST- Consuming APIs - RestTemplate - (getForEntity, getForObject, exchange)

--- Spring-review-17-REST-ConsumingAPIs/src/main/java/com/company/controller/Consume_RestTemplate.java
+++ Spring-review-17-REST-ConsumingAPIs/src/main/java/com/company/controller/Consume_RestTemplate.java
@@ -1,13 +1,14 @@
 package com.company.controller;
 
 import com.company.dto.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -35,6 +36,20 @@ public class Consume_RestTemplate {
         String URL = URI + "/{id}";
 
         return restTemplate.getForObject(URL,Object.class,id);
+    }
+
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumeDummyApi(){
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        httpHeaders.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/v1/user", HttpMethod.GET,httpEntity,Object.class);
+        return response;
+
     }
 
 }
