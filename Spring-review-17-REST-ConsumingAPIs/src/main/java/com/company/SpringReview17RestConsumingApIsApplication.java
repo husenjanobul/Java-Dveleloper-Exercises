ebcb6a8f913539485066c0f3245f0fa5b8d5f8bf
husenjanobul <husenjanobul@gmail.com>
2023-12-16 20:39:49 +0100

    REST- Consuming APIs

---
+++ Spring-review-17-REST-ConsumingAPIs/src/main/java/com/company/SpringReview17RestConsumingApIsApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReview17RestConsumingApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReview17RestConsumingApIsApplication.class, args);
+	}
+
+}
