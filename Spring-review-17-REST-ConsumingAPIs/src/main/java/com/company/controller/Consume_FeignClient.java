be54cbe82d414e0d7e34497a700ad2693afd87a9
husenjanobul <husenjanobul@gmail.com>
2023-12-18 10:44:40 +0100

    REST- @EnableFeignClients, @FeignClient

---
+++ Spring-review-17-REST-ConsumingAPIs/src/main/java/com/company/controller/Consume_FeignClient.java
@@ -0,0 +1,26 @@
+package com.company.controller;
+
+
+import com.company.client.UserClient;
+import com.company.dto.ResponseWrapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Consume_FeignClient {
+
+    private UserClient userClient;
+
+    public Consume_FeignClient(UserClient userClient) {
+        this.userClient = userClient;
+    }
+
+    @GetMapping("/api/v1/users")
+    public ResponseEntity<ResponseWrapper> getUsers(){
+
+        return ResponseEntity.ok(new ResponseWrapper("UserList Retrieved",userClient.getUsers()));
+
+    }
+
+}
